f4bdebc0ac486a77297ac33c2a3b32a38311483f
Paulo Cesar Ventura <paulo.pc.vs@gmail.com>
2022-11-15 17:27:57 -0500

    Use 'requests' library to download

--- fetch_cdc_truth_file.py
+++ fetch_cdc_truth_file.py
@@ -1,11 +1,29 @@
+"""
+Fetch the latest FluSight hospitalization data from the CDC Github repository.
+
+CDC Repo: https://raw.githubusercontent.com/cdcepi/Flusight-forecast-data
+
+Usage
+-----
+> python fetch_cdc_truth_file.py
+
+No arguments required, the file will already be saved in the correct directory (which is created if not existing).
+"""
+
 import os
+import sys
+
+import requests
 
 URL = "https://raw.githubusercontent.com/cdcepi/Flusight-forecast-data/master/data-truth/" \
       "truth-Incident%20Hospitalizations.csv"
-OUT_FNAME = '"hosp_data/truth-Incident Hospitalizations.csv"'
+OUT_FNAME = 'hosp_data/truth-Incident Hospitalizations.csv'
+TIMEOUT = 20  # Time in seconds to wait for the file request.
 
 
 def main():
+
+    # --- Preamble: make sure that the user wants to overwrite current file
     answer = input("You're about to download updated hospitalization data from CDC github.\n"
                    "Did you backup the previous file? (y/n)")
 
@@ -19,7 +37,33 @@ def main():
         print("Quitting now...")
         return
 
-    os.system(f"curl {URL} -o {OUT_FNAME}")
+    # --- Download commands
+    print(f"Downloading from... \n'{URL}'")
+
+    try:
+        # FILE REQUEST COMMAND
+        r: requests.Response = requests.get(URL, timeout=TIMEOUT)
+
+    except requests.exceptions.ConnectionError as e:
+        sys.stderr.write("\n\n#############################\nTHERE WAS A CONNECTION ERROR"
+                         "\n#############################\n\n")
+        print(e.args[0])
+        print("\nThe file was NOT downloaded.")
+        return
+
+    # Check status, raise error if unsucessful
+    r.raise_for_status()
+
+    # Otherwise, exports the resulting file.
+    print(f"Content successfully fetched, now saving into...\n{OUT_FNAME}")
+    os.makedirs(os.path.dirname(OUT_FNAME), exist_ok=True)
+    with open(OUT_FNAME, "w") as fp:
+        fp.write(r.text)
+
+    # # # -()- Simple solution: use curl. Requires that it is installed, though.
+    # os.system(f"curl {URL} -o \"{OUT_FNAME}\"")
+
+    print("Completed!")
 
 
 if __name__ == "__main__":
