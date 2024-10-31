260ad1c744ca9ac5574130b81ca32193d468f823
Paulo Cesar Ventura <42674294+paulocv@users.noreply.github.com>
2024-10-31 10:48:59 -0400

    More project restructures: cleanup root dir

--- activate_env.sh
+++ activate_env.sh
@@ -1,2 +1,5 @@
 # conda activate rtrend_forecast  # First version Rtrend
 conda activate rtrend_dev  # Dev environment for the Rtrend library 
+
+# Add source directories to PATH
+PATH="$PATH:$PWD/src_experimental:$PWD/src_operational"
---
+++ population_data/readme.md
@@ -0,0 +1,4 @@
+# DEPRECATED – Population data
+
+This folder used to contain demographic and populational data for the forecast.
+It has been moved to [aux_data](../aux_data), but kept for some time as legacy. 
